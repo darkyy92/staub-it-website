0a582f9f9fe41e547b91e8ae4eeeb86de9daef02
Joël <joel@staub-it.ch>
2025-01-10 01:58:29 +0100

    More partner logos

--- src/components/partners/Partners.tsx
+++ src/components/partners/Partners.tsx
@@ -7,31 +7,31 @@ const partners = [
   },
   {
     name: "Google Workspace",
-    logo: "/lovable-uploads/bee82b60-bc44-44cc-9f8b-c7e202430ec0.png",
+    logo: "/lovable-uploads/GoogleWorkspace_Logo_White.png",
   },
   {
     name: "Datto",
-    logo: "/lovable-uploads/03c9638f-4310-4299-921f-e2fbce62352d.png",
+    logo: "/lovable-uploads/datto-white.webp",
   },
   {
     name: "Kaseya",
-    logo: "/lovable-uploads/13c00a65-e2ed-457f-a1de-27e8a136c8a5.png",
+    logo: "/lovable-uploads/Kaseya_PNG-4x_White_Logo.png",
   },
   {
     name: "Jamf",
-    logo: "/lovable-uploads/338ac460-81e6-4c42-8a3b-2b7dcd0754a1.png",
+    logo: "/lovable-uploads/jamf-logo.png",
   },
   {
     name: "SonicWall",
-    logo: "/lovable-uploads/3db257cd-5cef-489a-a09a-e24e8732ee94.png",
+    logo: "/lovable-uploads/sonicwall-logo-white.png",
   },
   {
     name: "Webflow",
-    logo: "/lovable-uploads/55135e9d-8f12-4d79-9e49-7d164aeda7ec.png",
+    logo: "/lovable-uploads/webflow-logo-white.svg",
   },
   {
     name: "Ubiquiti",
-    logo: "/lovable-uploads/6f385a8e-8ea5-485e-91aa-0ec0da45e9c6.png",
+    logo: "/lovable-uploads/ubiquiti-white-logo-1024x309-1-300x143.png",
   },
 ];
 
